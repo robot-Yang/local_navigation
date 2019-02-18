b371bc5220fdfaffe585f4d401f99e846cd2a990
absorbraver <hy01841386@163.com>
2019-02-18 10:51:02 +0900

    Rename intel_ros_install.sh to intelRos_install.sh

---
+++ intelRos_install.sh
@@ -0,0 +1,10 @@
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src/
+
+catkin_init_workspace 
+cd ..
+catkin_make clean
+catkin_make -DCATKIN_ENABLE_TESTING=False -DCMAKE_BUILD_TYPE=Release
+catkin_make install
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
