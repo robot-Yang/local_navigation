23f26c75004dc0e837cdc2f50def88486aa686c2
Chen Yang <absorbraver@gmail.com>
2019-05-26 16:55:08 +0900

    Create test.py

---
+++ ADDA_upboard/test.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+
+import HighPrecision_ADDA as converter
+import time
+import math
+import threading
+
+conv = converter.AD_DA()
+
+while True:
+    b = conv.ReadChannel(0, conv.data_format.voltage)
+    a = conv.ReadChannel(1, conv.data_format.voltage)
+    c = conv.ReadChannel(5, conv.data_format.voltage)
+    d = conv.ReadChannel(6, conv.data_format.voltage)
+    e = conv.ReadChannel(7, conv.data_format.voltage)
+    a = float(a)
+    b = float(b)
+    c = float(c)
+    d = float(d)
+    e = float(e)
+    a = round(a, 2)
+    b = round(b, 2)
+    c = round(c, 2)
+    d = round(d, 2)
+    e = round(e, 2)
+
+    conv.SET_DAC0(2500, conv.data_format.voltage)
+    conv.SET_DAC1(1600, conv.data_format.voltage)
+
+    print("a=",a, "b=",b,"c=",c,"d=",d, "e=",e)
+
+    time.sleep(0.2)
